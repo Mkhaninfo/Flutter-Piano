b2f66fa1933a04dc69ec127914d858a1bbace1b0
Mkhaninfu <you@example.com>
2022-01-04 16:16:04 +0600

    piano

---
+++ lib/utils/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ThemeColors {
+  static const Color COLOR1 = Color(0xFF7b241c);
+  static const Color COLOR2 = Color(0xFFe74c3c);
+  static const Color COLOR3 = Color(0xFFf1948a);
+  static const Color COLOR4 = Color(0xFF5b2c6f);
+  static const Color COLOR5 = Color(0xFFa569bd);
+  static const Color COLOR6 = Color(0xFFc39bd3);
+}
